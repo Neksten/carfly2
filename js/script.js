6d8741018c3deca43fb31e314b5bd58c9a0d1dbe
Denis <89742455+Neksten@users.noreply.github.com>
2023-02-27 09:16:42 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Burger
+/*
+const menuBody = document.querySelector('.menu__body');
+const iconMenu = document.querySelector('.icon-menu');
+
+iconMenu.addEventListener('click', () => {
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+})
+*/
